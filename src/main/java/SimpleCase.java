67257a84f3baab2ec457b5546ddc742561459308
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2017-05-22 20:41:51 +0200

    Init project

---
+++ src/main/java/SimpleCase.java
@@ -0,0 +1,14 @@
+/**
+ * Created by aleksandar on 22/05/17.
+ */
+public class SimpleCase {
+
+    public static boolean alwaysTrue() {
+        return true;
+    }
+
+    public static boolean alwaysFalse() {
+        return false;
+    }
+
+}
---
+++ src/test/java/SimpleCaseTest.java
@@ -0,0 +1,17 @@
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aleksandar on 22/05/17.
+ */
+public class SimpleCaseTest {
+
+    @Test
+    public void shouldGiveAlwaysTrue() throws Exception {
+        assertThat(SimpleCase.alwaysFalse(), CoreMatchers.equalTo(false));
+    }
+
+}
